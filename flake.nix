438b94cfc37c600799d3ec0961e060ae626288ff
Joshua Potter <jrpotter2112@gmail.com>
2022-02-28 20:51:42 -0500

    Add maven repository.

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         path = ./haskell;
         description = "A minimal Haskell flake";
       };
+      maven = {
+        path = ./maven;
+        description = "A minimal Maven flake";
+      };
       rust = {
         path = ./rust;
         description = "A minimal Rust flake";
---
+++ maven/flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "A minimal Maven flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      localOverlay = final: prev:
+        let
+          # We package our entire repository as a fixed output derivation for
+          # use within nix. Refer to the called package for more information.
+          repo = prev.callPackage "${self}/build-maven-repo.nix" { };
+
+          hello-world-jar = prev.stdenv.mkDerivation rec {
+            pname = "hello-world";
+            version = "0.1.0";
+            src = self;
+            buildInputs = [ prev.maven ];
+            nativeBuildInputs = [ prev.makeWrapper ];
+            buildPhase = ''
+              runHook preBuild
+              mvn --offline -Dmaven.repo.local=${repo} package;
+              runHook postBuild
+            '';
+            installPhase = ''
+              runHook preInstall
+              install -Dm644 target/${pname}-${version}.jar $out/share/java
+              runHook postInstall
+            '';
+          };
+        in {
+          hello-world = prev.writeShellScriptBin "hello-world" ''
+            ${prev.jre}/bin/java -jar ${hello-world-jar}/share/java
+          '';
+        };
+
+      pkgsForSystem = system: import nixpkgs {
+        inherit system;
+        overlays = [ localOverlay ];
+      };
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system: with (pkgsForSystem system); {
+        packages = { inherit hello-world; };
+
+        defaultPackage = self.packages.${system}.hello-world;
+
+        devShell = mkShell {
+          buildInputs = lib.attrValues self.packages.${system} ++ [
+            google-java-format
+            groovy
+            maven
+            openjdk11
+          ];
+        };
+      }) // { overlay = localOverlay; };
+}

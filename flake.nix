49d89f5c9e5bc8d46ffdef529b8f13161bbe8fa2
Joshua Potter <jrpotter2112@gmail.com>
2022-03-03 18:17:34 -0500

    Python branch.

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A minimal Python flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
+    overlay = nixpkgs.lib.composeManyExtensions [
+      poetry2nix.overlay
+      (final: prev: {
+        hello-world = prev.poetry2nix.mkPoetryApplication {
+          projectDir = ./hello-world;
+          python = prev.python38;
+        };
+      })
+    ];
+  } // (flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      };
+    in
+    with pkgs; {
+      packages = { inherit hello-world; };
+
+      defaultPackage = self.packages.${system}.hello-world;
+
+      devShell = mkShell {
+        buildInputs = lib.attrValues self.packages.${system} ++ [
+          nodePackages.pyright
+          poetry
+          python38
+        ];
+      };
+    }));
+}

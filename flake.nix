9f2ccdb349d924f3e59691ac45be14285eff2e74
Joshua Potter <jrpotter2112@gmail.com>
2022-04-01 11:46:18 -0400

    Incorporate flake-compat.

--- flake.nix
+++ flake.nix
@@ -2,15 +2,19 @@
   description = "A minimal Python flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
+  outputs = { self, flake-compat, flake-utils, nixpkgs, poetry2nix }: {
     overlay = nixpkgs.lib.composeManyExtensions [
       poetry2nix.overlay
       (final: prev: {

0ac159f52daee983e72d4c39e0c8c4c476ea0328
Joshua Potter <jrpotter2112@gmail.com>
2022-04-01 12:24:07 -0400

    Setup haskell package in `haskellPackages`.

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -13,42 +13,54 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
   };
 
-  outputs = { self, flake-compat, flake-utils, nixpkgs }: {
-    overlay = final: prev: {
-      hello-world = prev.haskellPackages.callCabal2nix "hello-world" self { };
-    };
-  } // (flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, flake-compat, flake-utils, nixpkgs }:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
+      name = "hello-world";
+    in
+    {
+      overlay = final: prev: {
+        haskellPackages = prev.haskellPackages.override {
+          overrides = _self: _super: {
+            "${name}" = prev.haskellPackages.callCabal2nix name self { };
+          };
+        };
       };
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
 
-      haskell = {
-        ghc = pkgs.haskellPackages.ghc;
+        ghc = pkgs.haskellPackages.ghcWithPackages (hpkgs: [
+          hpkgs.cabal-install
+          # hpkgs.${name}
+        ]);
+
         hls = pkgs.haskell-language-server.override {
           supportedGhcVersions = [ "8107" ];
         };
-      };
-    in
-    with pkgs; {
-      packages = { inherit hello-world; };
+      in
+      with pkgs; {
+        packages = {
+          "${name}" = haskellPackages.callCabal2nix name self { };
+        };
 
-      defaultPackage = self.packages.${system}.hello-world;
+        defaultPackage = self.packages.${system}.${name};
 
-      devShell = mkShell {
-        buildInputs = [
-          haskell.ghc
-          haskell.hls
-          gdb
-          # GHC depends on LANG so need this package to properly interpret our
-          # files with e.g. tasty-discover.
-          # https://www.reddit.com/r/Nix/comments/jyczts/nixshell_locale_issue/
-          glibcLocales
-          haskellPackages.cabal-install
-          haskellPackages.ormolu
-          haskellPackages.tasty-discover
-        ];
-      };
-    }));
-}
+        devShell = mkShell {
+          buildInputs = [
+            gdb
+            ghc
+            hls
+            # Additional packages.
+            haskellPackages.ormolu
+            haskellPackages.tasty-discover
+            # GHC depends on LANG so need this package to properly interpret our
+            # files with e.g. tasty-discover.
+            # https://www.reddit.com/r/Nix/comments/jyczts/nixshell_locale_issue/
+            glibcLocales
+          ];
+        };
+      }));
+  }
